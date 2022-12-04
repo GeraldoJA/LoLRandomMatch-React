2156a58cf39fe88649b85ef342a7432b8f08873f
Geraldo <geraldo.gja@gmail.com>
2022-12-04 16:08:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+import bg from "./imgs/mapa.png";
+
+export default function App(){
+  return(
+    <div style={{backgroundImage: `url(${bg})`  }}>
+      <h1>LoL Random Match</h1>
+      
     </div>
-  );
+  )
 }
-
-export default App;
